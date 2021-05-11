fc081ea8a8e07c733f185c869719c7f7b409190a
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-05-11 21:20:50 +0100

    somechanges to UserProfile

---
+++ app/src/main/java/edu/kylegilmartin/teamup/Settings/FAQ.java
@@ -0,0 +1,16 @@
+package edu.kylegilmartin.teamup.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.kylegilmartin.teamup.R;
+
+public class FAQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_a_q);
+    }
+}
--- app/src/main/java/edu/kylegilmartin/teamup/UserDetails/UserProfile.java
+++ app/src/main/java/edu/kylegilmartin/teamup/UserDetails/UserProfile.java
@@ -1,13 +1,18 @@
 package edu.kylegilmartin.teamup.UserDetails;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import edu.kylegilmartin.teamup.LoginRegister.Login;
+import edu.kylegilmartin.teamup.MainActivity;
 import edu.kylegilmartin.teamup.R;
 import edu.kylegilmartin.teamup.admin.AdminMain;
 
@@ -19,6 +24,30 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.menuAccount);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menuHome:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.menuFavourite:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.menuSettings:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
         frameLayout = findViewById(R.id.changeProfile);
         frameLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +57,7 @@ public class UserProfile extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
