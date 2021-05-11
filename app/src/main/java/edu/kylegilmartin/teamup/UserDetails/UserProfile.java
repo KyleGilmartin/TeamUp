836c0ca03df3b174f269d68f936c15f6cc1275e6
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-05-11 22:04:16 +0100

    somechanges to UserProfile

--- app/src/main/java/edu/kylegilmartin/teamup/UserDetails/UserProfile.java
+++ app/src/main/java/edu/kylegilmartin/teamup/UserDetails/UserProfile.java
@@ -8,16 +8,29 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.kylegilmartin.teamup.LoginRegister.Login;
+import edu.kylegilmartin.teamup.LoginRegister.User;
 import edu.kylegilmartin.teamup.MainActivity;
 import edu.kylegilmartin.teamup.R;
 import edu.kylegilmartin.teamup.admin.AdminMain;
 
 public class UserProfile extends AppCompatActivity {
     private FrameLayout frameLayout;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +68,46 @@ public class UserProfile extends AppCompatActivity {
                 Intent i = new Intent(UserProfile.this, EditUserProfile.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+
+        final TextView fullnameTextView = findViewById(R.id.userfullname);
+        final TextView emailtextview = findViewById(R.id.emailuser);
+        final TextView consoletypetextview = findViewById(R.id.consoleuser);
+        final TextView nameidtextview = findViewById(R.id.nameiduser);
+        final TextView kdtextview = findViewById(R.id.kduser);
+        final TextView username = findViewById(R.id.usernameuser);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String consoletype = userProfile.consoletype;
+                    String kd = userProfile.kd;
+                    String nameID = userProfile.nameid;
+                    fullnameTextView.setText(fullName == fullName ? fullName : "No Data");
+                    username.setText(fullName == fullName ? fullName : "No Data");
+                    emailtextview.setText(email == email ? email : "No Data");
+                    consoletypetextview.setText(consoletype == consoletype ? consoletype : "No Data");
+                    nameidtextview.setText(nameID == nameID ? nameID : "No Data");
+                    kdtextview.setText(kd == kd ? kd : "No Data");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserProfile.this, "Something bad happened!", Toast.LENGTH_SHORT).show();
             }
         });
 
